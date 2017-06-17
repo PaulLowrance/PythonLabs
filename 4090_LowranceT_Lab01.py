e35c37161dd96f3469e8da3ec885cb4db48a8b67
Unknown <tpaul.lowrance@gmail.com>
2017-06-17 15:32:19 -0500

    Lab 01 Submission
    This the intial repo stand up and Lab 01 submission

---
+++ 4090_LowranceT_Lab01.py
@@ -0,0 +1,13 @@
+#Tim Lowrance
+#000420791
+#19 June 2017
+#17 June 2017
+'''
+Prints static information about the developer and shows
+that the installation of Python 3.6.1 was complete
+'''
+print("Preferred Name: Tim")
+print("""Favorite Movie: "Tombstone" ... or "Grandmas Boy".""")
+print("Favorite Band: Breaking Benjamin")
+print("""Favorite Book: "Jurassic Park", by Michael Crichton""")
+
